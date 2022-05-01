024c604becf6db4e71bc4068381d44f5bd5859ca
Roman Ac <roman25xx@gmail.com>
2022-05-01 23:15:45 +1000

    WIP portfolio page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,17 +1,43 @@
 import React from 'react';
+import ARK from '../assets/ARK.png'
+
 
 export default function Portfolio() {
+
+  const projectData = [
+    {
+      projectName: 'Act of Random Kindness',
+      image: ARK,
+      siteLink: 'https://act-of-random-kindness.herokuapp.com/',
+      repoLink: 'https://github.com/jcarait/act-of-random-kindness'
+    },
+    {
+      projectName: 'Cooped up Cooking',
+      image: ARK,
+      siteLink: 'https://act-of-random-kindness.herokuapp.com/',
+      repoLink: 'https://github.com/jcarait/act-of-random-kindness'
+    },
+  ]
+  const Project = ({ projectName, image, siteLink, repoLink }) => {
+    
+    return (
+        <div className="project-card">
+          <h3>{projectName}</h3>
+            <img className="project-examples" src={image} alt=''/>
+            <a className="button" href={siteLink}>Site</a>
+            <a className="button" href={repoLink}>Repo</a>
+        </div>
+      )
+};
+
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="project-container">      
+      <h1>Projects</h1>
+      <div className="projects" >
+        {projectData.map(({ projectName, image, siteLink, repoLink }) => (
+          <Project projectName={projectName} image={image} siteLink={siteLink} repoLink={repoLink} />
+        ))}
+      </div>
     </div>
   );
 }
