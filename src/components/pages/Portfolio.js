53d663d0c0d686eb25e43aeffc09c847b33dbc5a
Roman Ac <roman25xx@gmail.com>
2022-05-02 00:21:09 +1000

    portfolio complete

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ARK from '../assets/ARK.png'
+import ARK from '../assets/ARK.png';
+import CUC from '../assets/coopedupcooking.png';
+import WDB from '../assets/weatherdash.png';
+import WDS from '../assets/workdaysch.png';
 
 
 export default function Portfolio() {
@@ -13,19 +16,30 @@ export default function Portfolio() {
     },
     {
       projectName: 'Cooped up Cooking',
-      image: ARK,
-      siteLink: 'https://act-of-random-kindness.herokuapp.com/',
-      repoLink: 'https://github.com/jcarait/act-of-random-kindness'
+      image: CUC,
+      siteLink: 'https://oliverlkelly.github.io/cooped-up-cooking/',
+      repoLink: 'https://github.com/oliverlkelly/cooped-up-cooking'
+    },
+    {
+      projectName: 'Weather Dashboard',
+      image: WDB,
+      siteLink: 'https://roman-ac.github.io/weather-dashboard-by-roman-ac/',
+      repoLink: 'https://github.com/roman-ac/weather-dashboard-by-roman-ac'
+    },
+    {
+      projectName: 'Work Day Scheduler',
+      image: WDS,
+      siteLink: 'https://roman-ac.github.io/work-day-scheduler-by-roman-ac/',
+      repoLink: 'https://github.com/roman-ac/work-day-scheduler-by-roman-ac'
     },
   ]
   const Project = ({ projectName, image, siteLink, repoLink }) => {
     
     return (
         <div className="project-card">
-          <h3>{projectName}</h3>
-            <img className="project-examples" src={image} alt=''/>
-            <a className="button" href={siteLink}>Site</a>
-            <a className="button" href={repoLink}>Repo</a>
+          <h3><a className="project-name" href={siteLink}>{projectName}</a></h3>
+            <img className="project-image" src={image} alt=''/>
+            <a className="button" href={repoLink}>GitHub</a>
         </div>
       )
 };
