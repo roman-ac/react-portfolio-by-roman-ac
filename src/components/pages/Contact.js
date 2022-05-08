5d487528217c5224e73538ad33718a8271ca22db
Roman Ac <roman25xx@gmail.com>
2022-05-08 23:38:50 +1000

    contact & resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,23 +4,23 @@ export default function Contact() {
   return (
     <div className="contact-container">
       <h1 style={{textAlign: "center"}}>Contact</h1>
-      <div className="contact-contents">
+      <div className="contact-box">
         <div>
             <div>
-                <h3>Contact number</h3>
-                <a href="tel:+61419090527">+61 468 871 713</a>
+                <h3 className='contact-header'>Contact number</h3>
+                <a className='contact-content' href="tel:+61468871713">+61 468 871 713</a>
             </div>
             <div>
-                <h3>Email</h3>
-                <a href="mailto:roman25xx@gmail.com">roman25xx@gmail.com</a>
+                <h3 className='contact-header'>Email</h3>
+                <a className='contact-content' href="mailto:roman25xx@gmail.com">roman25xx@gmail.com</a>
             </div>
             <div>
-                <h3>Github</h3>
-                <a href="https://github.com/roman-ac/" target="blank">Roman Ac</a>
+                <h3 className='contact-header'>Github</h3>
+                <a className='contact-content' href="https://github.com/roman-ac/" target="blank">Roman Ac</a>
             </div>
             <div>
-                <h3>LinkedIn</h3>
-                <a href="https://www.linkedin.com/in/roman-acharya-326967222/" target="blank">Roman-Acharya</a>
+                <h3 className='contact-header'>LinkedIn</h3>
+                <a className='contact-content' href="https://www.linkedin.com/in/roman-acharya-326967222/" target="blank">Roman-Acharya</a>
             </div>
             
         </div>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page,pdfjs } from 'react-pdf';
+import cvroman from '../assets/cvroman.pdf'
 
 export default function Resume() {
+  pdfjs.GlobalWorkerOptions.workerSrc = 
+  `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  
+  /*To Prevent right click on screen*/
+  document.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+  });
+    
+  /*When document gets loaded successfully*/
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
+  
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+  
+  function previousPage() {
+    changePage(-1);
+  }
+  
+  function nextPage() {
+    changePage(1);
+  }
+  
   return (
-    <div>
-      <h1 style={{textAlign: "center"}}>Resume</h1>
-      <p>
-        
-      </p>
-    </div>
+    <>
+    <div className="main">
+      <Document
+        file={cvroman}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div>
+        <div className="pagec">
+          Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+        </div>
+        <div className="buttonc">
+        <button
+          type="button"
+          disabled={pageNumber <= 1}
+          onClick={previousPage}
+          className="Pre"
+            
+        >
+          Previous
+        </button>
+        <button
+          type="button"
+          disabled={pageNumber >= numPages}
+          onClick={nextPage}
+           
+        >
+          Next
+        </button>
+        </div>
+      </div>
+      </div>
+    </>
   );
 }
